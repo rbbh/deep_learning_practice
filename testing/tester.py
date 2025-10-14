2d77f99824b27830e82accf228bd1ac8aa8af89a
Rafael Holanda <holandaraf@gmail.com>
2025-10-14 20:50:48 -0300

    Added image visualization.

--- testing/tester.py
+++ testing/tester.py
@@ -1,5 +1,6 @@
 import logging
 import matplotlib.pyplot as plt
+import numpy as np
 import seaborn as sns
 import torch
 from sklearn.metrics import classification_report, confusion_matrix
@@ -46,4 +47,34 @@ class Tester:
         plt.xlabel("Predicted")
         plt.ylabel("True")
         plt.title("Confusion Matrix")
+        plt.show()
+
+        self._visualize_predictions()
+
+    def _visualize_predictions(self, num_images=25):
+        # Get a batch of test data
+        dataiter = iter(self.test_loader)
+        images, labels = next(dataiter)
+        images_for_viz = images.cpu().numpy()
+        images, labels = images.to(self.device), labels.to(self.device)
+
+        # Get predictions
+        outputs = self.model(images)
+        _, predicted = torch.max(outputs, 1)
+
+        # Plot images with predictions
+        fig = plt.figure(figsize=(15, 7))
+        for idx in np.arange(num_images):
+            if idx >= len(images):
+                break
+            ax = fig.add_subplot(
+                (num_images // 5) + 1, 5, idx + 1, xticks=[], yticks=[]
+            )
+            # Un-normalize
+            img = images_for_viz[idx] / 2 + 0.5
+            plt.imshow(np.transpose(img, (1, 2, 0)))
+            ax.set_title(
+                f"{self.classes[predicted[idx].item()]} ({self.classes[labels[idx].item()]})",
+                color=("green" if predicted[idx] == labels[idx] else "red"),
+            )
         plt.show()
